3d2b1cdeca5bb23d4b22f602f075dcc3bcd50883
Guilherme Almeida de Menezes <gam@CESAR-LDAP.INTRANET>
2016-05-17 12:13:05 -0300

    project

---
+++ TesteMqtt/Model/CommandHeader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteMqtt.Model
+{
+    class CommandHeader
+    {
+
+        public string CloudDomain { get; set; }
+        public long HouseHoldID { get; set; }
+        public long LocationID { get; set; }
+        public MessageTypeEnum MessageType { get; set; }
+        public MessageSubjectEnum MessageSubject { get; set; }
+        public long MessageSubjectID { get; set; }
+
+        public enum MessageTypeEnum { CONTROL, STATUS }
+        public enum MessageSubjectEnum { ENDPOINTS }
+    }
+}
---
+++ TesteMqtt/Model/EndpointState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteMqtt.Model
+{
+    class EndpointState
+    {
+        public bool Powered { get; set; }
+        public float Brightness { get; set; }
+        public int Hue { get; set; }
+        public int Saturation { get; set; }
+        public int Kelvin { get; set; }
+        public AbilityEnum Ability { get; set; }
+
+        public enum AbilityEnum { DIM, COLOR, NONE }
+
+    }
+}
